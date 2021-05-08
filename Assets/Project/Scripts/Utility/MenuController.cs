64cc32ff116634abf582b2596f1a20cea1688b32
Alvar <alvarcoll310@gmail.com>
2021-05-08 21:41:05 +0200

    Created Menu Scene

---
+++ Assets/Project/Scripts/Utility/MenuController.cs
@@ -0,0 +1,12 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class MenuController : MonoBehaviour
+{
+    private void Start()
+    {
+        Cursor.lockState = CursorLockMode.None;
+        Cursor.visible = true;
+    }
+}
