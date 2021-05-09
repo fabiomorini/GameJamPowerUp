5190b9c0dfde6356be7af27cfa3155210838419e
Alvar <alvarcoll310@gmail.com>
2021-05-09 19:54:36 +0200

    a

--- Assets/Project/Scripts/Utility/CutSceneController.cs
+++ Assets/Project/Scripts/Utility/CutSceneController.cs
@@ -1,11 +1,13 @@
 ﻿using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using UnityEngine.SceneManagement;
 
 public class CutSceneController : MonoBehaviour
 {
     private bool garsa = false;
     private bool drac = false;
+    private float timer = 10.0f;
 
     private void Start()
     {
@@ -28,6 +30,16 @@ public class CutSceneController : MonoBehaviour
         else
         {
             GameObject.Find("DracCutScene").SetActive(false);
+        }
+    }
+
+    private void Update()
+    {
+        timer -= Time.deltaTime;
+
+        if(timer <= 0)
+        {
+            SceneManager.LoadScene("Final");
         }
     }
 }
