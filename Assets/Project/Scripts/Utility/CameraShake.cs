c5772f0c1d092ed6c0831b7bfd0d99bbf2bc53a1
Fabio <Morini>
2021-05-09 16:24:28 +0200

    menuFinal

--- Assets/Project/Scripts/Utility/CameraShake.cs
+++ Assets/Project/Scripts/Utility/CameraShake.cs
@@ -10,5 +10,4 @@ public class CameraShake : MonoBehaviour
     {
         canAnim.SetTrigger("Shake");
     }
-
 }
---
+++ Assets/Project/Scripts/Utility/MenuManager.cs
@@ -0,0 +1,33 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+
+public class MenuManager : MonoBehaviour
+{
+
+    public GameObject credits;
+    public GameObject buttons;
+
+
+    public void Exit()
+    {
+        Application.Quit();
+    }
+
+    public void PlayLevel()
+    {
+        SceneManager.LoadScene(SceneManager.GetActiveScene().buildIndex + 1);
+    }
+
+    public void Credits()
+    {
+        buttons.SetActive(false);
+        credits.SetActive(true);
+    }
+    public void Return()
+    {
+        buttons.SetActive(true);
+        credits.SetActive(false);
+    }
+}
